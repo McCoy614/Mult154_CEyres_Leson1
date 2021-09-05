4e540d79f7c03920d2dc544f4792c0119624d469
McCoy614 <834446@gmail.com>
2021-09-05 11:08:32 -0700

    Added the river, player, lily pads, veggie. For some reason, item script will not work.

---
+++ Assets/Scripts/ItemScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    public enum VegetableType
+    {
+        beet, carrot, radish
+    }
+
+    public VegetableType typeOfVeggie;
+}
+
